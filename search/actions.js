0dd946f69c0f8088cb5bcc2ea8b0508905cd3887
Steven Yildirim <spysteven@gmail.com>
2017-04-02 19:09:02 -0400

    javascript checkboxes

--- search/actions.js
+++ search/actions.js
@@ -1,73 +1,25 @@
+var FILTERS = [
+  'neutral',
+  'druid',
+  'mage',
+  'paladin',
+  'warrior',
+  'hunter',
+  'shaman',
+  'priest',
+  'rogue'
+];
 
-var document = window.document,
-    $_window = $( window ),
+//Function to handle filters by checking which are true and which are false
+function filterHandling() {
+  var filter = {};
+  FILTERS.forEach(function(f) {
+    filter[f] = $('#' + f).is(':checked');
+  });
+  console.log(filter);
+}
 
-    _results = [
-      'pyroblast'
-    ],
-
-      _rEscapeChars = /\/|\\|\.|\||\*|\&|\+|\(|\)|\[|\]|\?|\$|\^/g,
-      _rMatch = /[A-Z]?[a-z]+|[0-9]+/g,
-
-      _keys = [
-      13,
-      9
-    ],
-
-      _length = _results.length,
-
-      $_result = $( '.result' ),
-      $_search = $( '.search-bar' ),
-      $_searchContainer = $( '.search-container' ),
-
-      _resultPlaceholder = $_result.val();
-
-$_search.on( "keydown", function ( e ) {
-
-  if ( _keys.indexOf( e.keyCode ) !== -1 ) {
-
-    $_search.val( $_result.val() );
-
-    return false;
-
-  }
-
-}).on( "keyup", function () {
-
-  var value = $_search.val().replace( _rEscapeChars, "" ),
-      regex = new RegExp( "^"+value, "i" ),
-      matches = [];
-
-
-  if ( value ) {
-
-    for ( var i = _length; i--; ) {
-
-      if ( regex.test( _results[ i ] ) ) {
-
-        matches.push( _results[ i ] );
-
-      } else {
-
-        $_result.val( "" );
-
-      }
-
-    }
-
-    if ( matches.length ) {
-
-      for ( var i = matches.length; i--; ) {
-
-        $_result.val( matches[ i ] );
-
-      }
-
-    }
-
-  } else {
-
-    $_result.val( _resultPlaceholder );
-
-  }
-})
+//Loop to create change events for each filter checkbox
+FILTERS.forEach(function(filter) {
+  $('#' + filter).change(filterHandling);
+});
