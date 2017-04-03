513ba7a728b7fb12e1b8bb323b358d92075c4ce5
Steven Yildirim <spysteven@gmail.com>
2017-04-02 21:33:53 -0400

    filter javascript works
    sends  a map of arrays. Each array for each kind of filter(class, hp,
    etc) and in the array are the true filter values

--- searchCards/actions.js
+++ searchCards/actions.js
@@ -1,5 +1,5 @@
-var FILTERS = [
-  'neutral',
+var FILTERS = {
+  classes: ['neutral',
   'druid',
   'mage',
   'paladin',
@@ -7,19 +7,94 @@ var FILTERS = [
   'hunter',
   'shaman',
   'priest',
-  'rogue'
-];
+  'rogue'],
+  mana: [
+  'mana0',
+  'mana1',
+  'mana2',
+  'mana3',
+  'mana4',
+  'mana5',
+  'mana6',
+  'mana7',
+  'mana8',
+  'mana9',
+  'mana10'],
+  attack: [
+  'atk0',
+  'atk1',
+  'atk2',
+  'atk3',
+  'atk4',
+  'atk5',
+  'atk6',
+  'atk7',
+  'atk8',
+  'atk9',
+  'atk10'],
+  health: [
+  'hp0',
+  'hp1',
+  'hp2',
+  'hp3',
+  'hp4',
+  'hp5',
+  'hp6',
+  'hp7',
+  'hp8',
+  'hp9',
+  'hp10' ],
+  races: [
+  'beast',
+  'murloc',
+  'demon',
+  'mech',
+  'totem',
+  'pirate',
+  'dragon']
+};
 
-//Function to handle filters by checking which are true and which are false
+//Function to handle filters by creating a map called 'filter' with filter name and bool value
 function filterHandling() {
-  var filter = {};
-  FILTERS.forEach(function(f) {
-    filter[f] = $('#' + f).is(':checked');
+  var filter = {
+    classes: [],
+    mana: [],
+    attack: [],
+    health: [],
+    races: [],
+    name: ''
+  };
+  FILTERS.classes.forEach(function(f) {
+    if ($('#' + f).is(':checked')) {
+      filter.classes.push(f);
+    }
+  });
+  FILTERS.races.forEach(function(f) {
+    if ($('#' + f).is(':checked')) {
+      filter.races.push(f);
+    }
+  });
+  FILTERS.mana.forEach(function(f, index) {
+    if ($('#' + f).is(':checked')) {
+      filter.mana.push(index);
+    }
+  });
+  FILTERS.attack.forEach(function(f, index) {
+    if ($('#' + f).is(':checked')) {
+      filter.attack.push(index);
+    }
+  });
+  FILTERS.health.forEach(function(f, index) {
+    if ($('#' + f).is(':checked')) {
+      filter.health.push(index);
+    }
   });
   console.log(filter);
 }
 
 //Loop to create change events for each filter checkbox
-FILTERS.forEach(function(filter) {
-  $('#' + filter).change(filterHandling);
+Object.keys(FILTERS).forEach(function(key) {
+  FILTERS[key].forEach(function(filter) {
+    $('#' + filter).change(filterHandling);
+  });
 });
